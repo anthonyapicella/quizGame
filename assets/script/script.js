40eee7bac068e55b83acb914c99854d84c858bb5
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-02-18 18:57:16 -0800

    Remove img and audio assets, add new button and card styling

---
+++ assets/script/script.js
@@ -0,0 +1,21 @@
+/*
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+*/
+$display-font-sizes: (
+    1: 5rem,
+    2: 4.5rem,
+    3: 4rem,
+    4: 3.5rem,
+    5: 3rem,
+    6: 2.5rem
+  );
