1663da5cdb76e84e73f6c3b5d11147d5b8efcbe4
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-02-18 19:55:06 -0800

    Begin JS, add start function

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,11 +11,17 @@ THEN the game is over
 WHEN the game is over
 THEN I can save my initials and my score
 */
-$display-font-sizes: (
-    1: 5rem,
-    2: 4.5rem,
-    3: 4rem,
-    4: 3.5rem,
-    5: 3rem,
-    6: 2.5rem
-  );
+
+// declare variable for start button corresponding function
+var startButton = document.getElementById('start-btn');
+
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz() {
+
+}
+
+function selectAnswer(){
+
+}
+
